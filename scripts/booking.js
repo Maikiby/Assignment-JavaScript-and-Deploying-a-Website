6df2e0e9dd0b0dd4344487627ac3957af73dce50
Maikiby <maikiby123@gmail.com>
2022-12-15 17:42:58 -0700

    Implemented full and half buttons

--- scripts/booking.js
+++ scripts/booking.js
@@ -7,6 +7,11 @@ let dayCost = 0
 
 let dayCount = 0
 
+let dayRate = 35
+
+let half = document.getElementById("half") 
+
+let full = document.getElementById("full")
 /********* colour change days of week *********/
 // when the day buttons are clicked, we will apply the "clicked" class to that element, and update any other relevant variables. Then, we can recalculate the total cost.
 // added challenge: don't update the dayCounter if the same day is clicked more than once. hint: .classList.contains() might be helpful here!
@@ -47,14 +52,21 @@ clearButton.onclick = () => {
 /********* change rate *********/
 // when the half-day button is clicked, set the daily rate to $20, add the "clicked" class to the "half" element, remove it from the "full" element, and recalculate the total cost.
 
-
-
+half.onclick = () => {
+    dayRate = 20
+    half.classList.add("clicked")
+    full.classList.remove("clicked")
+    //math
+}
 
 // when the full-day button is clicked, the daily rate is set back to $35, the clicked class is added to "full" and removed from "half", and the total cost is recalculated.
 
-
-
-
+full.onclick = () => {
+    dayRate = 35
+    half.classList.remove("clicked")
+    full.classList.add("clicked")
+    //math
+}
 
 /********* calculate *********/
 // when a calculation is needed, set the innerHTML of the calculated-cost element to the appropriate value
